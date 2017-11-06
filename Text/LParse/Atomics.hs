028ca5df8e1e4467ca19aba208ae0e366f844e1c
Marcus Völker <voelker@embedded.rwth-aachen.de>
2017-11-06 10:59:00 +0100

    Fixed empty consume bug

--- Text/LParse/Atomics.hs
+++ Text/LParse/Atomics.hs
@@ -26,7 +26,7 @@ tokenReturn :: Parser r [a] a
 tokenReturn = tokenParse id
 
 consume :: (Eq t, Show t) => [t] -> Parser r [t] ()
-consume pre = cParse (all id . zipWith (==) pre) (pParse (drop (length pre)) noop) ("Expected " ++ show pre)
+consume pre = cParse ((&&) <$> (all id . zipWith (==) pre) <*> ((>= length pre) . length)) (pParse (drop (length pre)) noop) ("Expected " ++ show pre)
 
 consumeSingle :: (Eq t, Show t) => t -> Parser r [t] ()
 consumeSingle t = cParse (\s -> not (null s) && head s == t) (pParse tail noop) ("Expected " ++ show t)
