01c01ae18249571d4efd4258399e03d8e4f5cd23
Marcus Völker <voelker@embedded.rwth-aachen.de>
2017-11-09 13:46:46 +0100

    Fail tests

--- test/Test.hs
+++ test/Test.hs
@@ -14,13 +14,26 @@ succCases = [
     (noop,""),
     (eoi,""),
     (discard,"lel"),
-    ((discard >> eoi),"lorem ipsum"),
-    ((consume "prefix"),"prefixed")
+    (discard >> eoi,"lorem ipsum"),
+    (consume "prefix","prefixed"),
+    (consume "", "foo"),
+    (consume "", "")
     ]
 
+failCases = [
+    (eoi,"foo"),
+    (consume "prefix", "freepix"),
+    (consume "prefix", "")
+    ]
+
+runTests = map (uncurry doParse)
+
 main = 
-    let res = map (uncurry doParse) succCases in
-    if all isRight res then
+    let sres = runTests succCases in
+    let fres = runTests failCases in
+    if all isRight sres && all isLeft fres then
         exitSuccess
-    else
-        putStrLn (head $ lefts res) >> exitFailure
+    else if any isLeft sres then
+        putStrLn (head $ lefts sres) >> exitFailure
+    else 
+        putStrLn "Fail case succeeded" >> exitFailure
